02593dfd73b4aa7c9ee774ef4bf3fd778cfad4eb
问鼎 <postmaster@vpsv.cn>
2018-11-04 17:30:42 +0800

    用户密码加盐

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+# 密码散列
 
 # 定义模型
 class Role(db.Model):
@@ -12,12 +14,26 @@ class Role(db.Model):
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
+
 class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(64), unique=True, index=True)
 	# 模型关联
 	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+
+	# 当设置User().password的时候出发该方法
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 	# Python中这个_repr_函数，对应repr(object)这个函数，返回一个可以用来表示对象的可打印字符串
 	def __repr__(self):
